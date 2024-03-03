83a4e3a800f2403147b198674c15184a04c4a46f
thepoy <thepoy@163.com>
2024-03-03 09:52:06 +0800

    组件代码和样式分离到各自目录

--- vite.config.ts
+++ vite.config.ts
@@ -21,28 +21,47 @@ export default defineConfig({
   ],
 
   build: {
+    cssCodeSplit: true,
     // 打包输出的目录
     outDir: "lib",
     // 防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制
-    cssTarget: "chrome61",
+    // cssTarget: "chrome61",
     lib: {
       // 组件库源码的入口文件
       entry: resolve("packages/index.ts"),
       // 组件库名称
-      name: "alley-components",
+      // name: "alley-components",
       // 文件名称, 打包结果举例: my-packages.umd.cjs
-      fileName: "alley",
+      // fileName: "alley",
+      // formats: ["es", "umd"],
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ["solid-js"],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        // globals: {
-        //   react: "react",
-        //   "react-dom": "react-dom",
+      external: ["solid-js", "solid-icons", /node_modules/],
+      input: ["packages/index.ts"],
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          preserveModules: true,
+          dir: "lib",
+          preserveModulesRoot: "packages",
+        },
+        // {
+        //   format: "cjs",
+        //   entryFileNames: "[name].js",
+        //   preserveModules: true,
+        //   dir: "lib",
+        //   preserveModulesRoot: "",
         // },
-      },
+      ],
+      // output: {
+      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+      // globals: {
+      //   react: "react",
+      //   "react-dom": "react-dom",
+      // },
+      // },
     },
   },
 });
