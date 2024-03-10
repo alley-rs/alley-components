b5ddc65e466238ab194468ffab4cedd00b155856
Ryan Wang <thepoy@163.com>
2024-03-10 18:39:43 +0800

    feat(float-button): 添加回到顶部浮动按钮

---
+++ packages/components/float-button/index.tsx
@@ -0,0 +1,17 @@
+import { Component } from "solid-js";
+import Button from "./button";
+import Group from "./group";
+import type { FloatButtonProps } from "./interface";
+import BackTop from "./back-top";
+
+interface FloatButtonType extends Component<FloatButtonProps> {
+  Group: typeof Group;
+  BackTop: typeof BackTop;
+}
+
+const FloatButton = Button as FloatButtonType;
+
+FloatButton.Group = Group;
+FloatButton.BackTop = BackTop;
+
+export default FloatButton;
