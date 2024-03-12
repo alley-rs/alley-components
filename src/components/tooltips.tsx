ad4a7118c20311c22a91f9e0db7b5df48ddcf5c0
Ryan Wang <thepoy@163.com>
2024-03-12 15:26:29 +0800

    chore(button): 错误使用圆形按钮时控制台中打印警告

--- src/components/tooltips.tsx
+++ src/components/tooltips.tsx
@@ -1,27 +1,37 @@
-import { Tooltip, Button, Flex } from "../../packages";
+import { AiOutlineDown, AiOutlineUp } from "solid-icons/ai";
+import { Tooltip, Button, Flex, Alert } from "../../packages";
 
-const Alerts = () => (
-  <Flex gap={16} style={{ "margin-top": "40px" }} justify="round">
-    <Tooltip text="这是一条文字提示" placement="top">
-      <Button shape="circle">上</Button>
-    </Tooltip>
+const Tooltips = () => (
+  <>
+    <Alert
+      showIcon
+      type="warning"
+      message="本页面演示了圆形按钮的错误用法，不可将文字传入圆形按钮。可在控制台中查看警告信息。"
+    />
+    <Flex gap={16} style={{ "margin-top": "40px" }} justify="round">
+      <Tooltip text="这是一条文字提示" placement="top">
+        <Button shape="circle" icon={<AiOutlineUp />}>
+          上
+        </Button>
+      </Tooltip>
 
-    <Tooltip text="这是一条文字提示" placement="bottom">
-      <Button shape="circle" size="small">
-        下
-      </Button>
-    </Tooltip>
+      <Tooltip text="这是一条文字提示" placement="bottom">
+        <Button shape="circle" size="small" icon={<AiOutlineDown />}>
+          下
+        </Button>
+      </Tooltip>
 
-    <Tooltip text="这是一条文字提示" placement="left">
-      <Button shape="circle" size="large">
-        左
-      </Button>
-    </Tooltip>
+      <Tooltip text="这是一条文字提示" placement="left">
+        <Button shape="circle" size="large">
+          左
+        </Button>
+      </Tooltip>
 
-    <Tooltip text="这是一条文字提示" placement="right">
-      <Button>右</Button>
-    </Tooltip>
-  </Flex>
+      <Tooltip text="这是一条文字提示" placement="right">
+        <Button type="primary">右</Button>
+      </Tooltip>
+    </Flex>
+  </>
 );
 
-export default Alerts;
+export default Tooltips;
