1e4365bbd9f8a02cc506849b27e53429becce41c
Ryan Wang <thepoy@163.com>
2024-07-18 11:12:56 +0800

    chore(demo): tooltip 示例中添加延时显示

--- src/components/tooltips.tsx
+++ src/components/tooltips.tsx
@@ -1,5 +1,5 @@
 import { AiOutlineDown, AiOutlineUp } from "solid-icons/ai";
-import { Tooltip, Button, Flex, Alert } from "../../packages";
+import { Tooltip, Button, Flex, Alert, Divider } from "../../packages";
 
 const Tooltips = () => (
   <>
@@ -8,6 +8,9 @@ const Tooltips = () => (
       type="warning"
       message="本页面演示了圆形按钮的错误用法，不可将文字传入圆形按钮。可在控制台中查看警告信息。"
     />
+
+    <Divider>即时显示</Divider>
+
     <Flex gap={16} style={{ "margin-top": "40px" }} justify="round">
       <Tooltip text="这是一条文字提示" placement="top" size="small">
         <Button shape="circle" icon={<AiOutlineUp />}>
@@ -31,6 +34,14 @@ const Tooltips = () => (
         <Button type="primary">右</Button>
       </Tooltip>
     </Flex>
+
+    <Divider>延时显示</Divider>
+
+    <Tooltip text="关闭" placement="right" delay={1000}>
+      <Button shape="circle" size="large">
+        X
+      </Button>
+    </Tooltip>
   </>
 );
 
